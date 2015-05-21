6ead311f42333f0f37a68d164c0ae483f11a651b
Adam Eichelkraut <ake0005@tigermail.auburn.edu>
2015-05-21 16:04:02 -0500

    Created initial files and setup for unrolling refactoring

---
+++ edu.auburn.oaccrefac.ui/src/edu/auburn/oaccrefac/internal/ui/refactorings/LoopUnrollingRefactoring.java
@@ -0,0 +1,48 @@
+package edu.auburn.oaccrefac.internal.ui.refactorings;
+
+import org.eclipse.cdt.core.model.ICElement;
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
+
+/**
+ * This class defines the implementation for refactoring a loop
+ * so that it is unrolled. For example:
+ * 
+ * ORIGINAL:				REFACTORED:
+ * int x;					|  int x;
+ * for (x=0; x<100; x++)	|  for (x=0; x<100; x+=5) {
+ *   delete(x);				|    delete(x);
+ *   						|    delete(x+1);
+ *   						|	 delete(x+2);
+ *   						|	 delete(x+3);
+ *  						|	 delete(x+4);
+ *  						|  }
+ * (Example taken from Wikipedia's webpage on loop unrolling)
+ */
+@SuppressWarnings("restriction")
+public class LoopUnrollingRefactoring extends ForLoopRefactoring {
+
+	public LoopUnrollingRefactoring(ICElement element, ISelection selection, ICProject project) {
+		super(element, selection, project);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void collectModifications(IProgressMonitor pm, ModificationCollector collector)
+			throws CoreException, OperationCanceledException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected RefactoringDescriptor getRefactoringDescriptor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
