08a2d529d992c01c4fa9aa380961fe499dd42111
Jeffrey Overbey <com-github@jeff.over.bz>
2015-08-26 14:29:11 -0500

    Added copyright headers

--- edu.auburn.oaccrefac.cli/src/Main.java
+++ edu.auburn.oaccrefac.cli/src/Main.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Auburn University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jeff Overbey - initial API and implementation
+ *******************************************************************************/
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +30,9 @@ import edu.auburn.oaccrefac.core.change.FizzLoops;
 import edu.auburn.oaccrefac.core.change.IASTRewrite;
 import edu.auburn.oaccrefac.internal.core.ASTUtil;
 
+/**
+ * Command line driver.
+ */
 public class Main {
 
     public static void main(String[] args) {
--- edu.auburn.oaccrefac.cli/src/edu/auburn/oaccrefac/cli/dom/rewrite/package-info.java
+++ edu.auburn.oaccrefac.cli/src/edu/auburn/oaccrefac/cli/dom/rewrite/package-info.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Auburn University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jeff Overbey - initial API and implementation
+ *******************************************************************************/
+
 /**
  * This package contains replacements for some of CDT's classes for AST rewriting to avoid dependencies on the
  * org.eclipse.core.resources plug-in.
