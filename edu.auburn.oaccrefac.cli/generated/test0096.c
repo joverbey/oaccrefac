05d4b33008a6c0d1444c1667c8f41a34ea4cf572
Jeffrey Overbey <com-github@jeff.over.bz>
2015-09-15 10:13:25 -0500

    Added CLI generated loops and Unroll tests

---
+++ edu.auburn.oaccrefac.cli/generated/test0096.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+static float matrix1[10][10], matrix2[10][10];
+
+static void init();
+static void print();
+
+int main() {
+	init();
+	// AUTOTUNE
+	for (int i = 1; i < 9; i++) {
+		for (int j = 1; j < 9; j++) {
+			for (int k = 1; k < 9; k++) {
+				matrix1[i][j - 1] = 1.0;
+				matrix1[k - 1][i] = matrix2[j + 1][k - 1];
+			}
+		}
+	}
+	print();
+}
+
+static void init() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			matrix1[i][j] = 10 * i + j;
+			matrix2[i][j] = -10 * j - i;
+		}
+	}
+}
+
+static void print() {
+	printf("matrix1 =");
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf(" %2.2f", matrix1[i][j]);
+		}
+		printf("\n         ");
+	}
+	printf("\n");
+	printf("matrix2 =");
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf(" %2.2f", matrix2[i][j]);
+		}
+		printf("\n         ");
+	}
+	printf("\n");
+}
