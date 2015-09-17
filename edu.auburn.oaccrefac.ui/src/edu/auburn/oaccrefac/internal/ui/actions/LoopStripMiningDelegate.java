bea355b9df83d030bbacdd1f6a18d4e66bc6f6c7
zok0001 <zok0001@auburn.edu>
2015-09-17 13:35:38 -0500

    Removed commented out code in UI delegates.

--- edu.auburn.oaccrefac.ui/src/edu/auburn/oaccrefac/internal/ui/actions/LoopStripMiningDelegate.java
+++ edu.auburn.oaccrefac.ui/src/edu/auburn/oaccrefac/internal/ui/actions/LoopStripMiningDelegate.java
@@ -13,9 +13,7 @@ package edu.auburn.oaccrefac.internal.ui.actions;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.core.model.IWorkingCopy;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
 
