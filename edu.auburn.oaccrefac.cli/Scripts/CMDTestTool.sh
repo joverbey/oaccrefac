ea07b60808a7db8d04af83ac5d3dec5f54e430f9
galanite <blitzchamp13@gmail.com>
2016-02-08 11:44:22 -0600

    CMD Base Tool finished

--- edu.auburn.oaccrefac.cli/Scripts/CMDTestTool.sh
+++ edu.auburn.oaccrefac.cli/Scripts/CMDTestTool.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-cd ~/Code/oaccrefac/edu.auburn.oaccrefac.cli/
+curdir=`basename "$PWD"`
+if [ "$curdir" != "edu.auburn.oaccrefac.cli" ]
+then
+	echo "Error: This script must be run from the edu.auburn.oaccrefac.cli directory."
+	exit
+fi
 
 outerinner[0]="outer"
 outerinner[1]="inner"
 outerinner[2]="inner2"
 
-> ./Scripts/errorlog.txt
-> ./Scripts/output.txt
+>./Scripts/errorlog.txt
+>./Scripts/output.txt
+>./Scripts/outputtemp.txt
+cp examples/level1-CMD.c ./Scripts/inputtemp.txt
 
 for i in $(seq 77)
 do
@@ -17,11 +24,14 @@ do
 	for j in "${outerinner[@]}"
 	do
 		loopname+="$j"
-		java -cp "lib/*:bin:../edu.auburn.oaccrefac.core/bin" DistributeLoops examples/level1-CMD.c "$loopname"  > ./Scripts/output.txt 2>> ./Scripts/errorlog.txt
-		echo "Exit code is $?"
+		java -cp "lib/*:bin:../edu.auburn.oaccrefac.core/bin" DistributeLoops ./Scripts/inputtemp.txt "$loopname"  > ./Scripts/outputtemp.txt 2>> ./Scripts/errorlog.txt	
+		if [[ -s ./Scripts/outputtemp.txt ]]; then
+			cp ./Scripts/outputtemp.txt ./Scripts/output.txt
+		fi
 		echo $loopname
 		loopname=$loopnameTemp
 	done
 done
 
-
+rm ./Scripts/outputtemp.txt
+rm ./Scripts/inputtemp.txt
