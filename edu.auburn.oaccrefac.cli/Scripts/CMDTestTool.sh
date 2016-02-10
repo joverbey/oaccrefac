23bbb2bc4e3ddb1fe3f56ceb1aae7056686ea2c4
Jacob Neeley <blitzchamp13@gmail.com>
2016-02-10 10:58:51 -0600

    To Shelby 2/10/16

--- edu.auburn.oaccrefac.cli/Scripts/CMDTestTool.sh
+++ edu.auburn.oaccrefac.cli/Scripts/CMDTestTool.sh
@@ -12,13 +12,13 @@ outerinner[2]="inner2"
 
 >./Scripts/errorlog.txt
 >./Scripts/output.c
->./Scripts/outputtemp.c
-cp /home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/level1-CMD.c /home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/inputtemp.c
+>./examples/testcode-epcc/outputtemp.c
+cp ./examples/testcode-epcc/level1-CMD.c ./examples/testcode-epcc/inputtemp.c
 
 
 
-g++ /home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/level1-CMD.c /home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/inputtemp.c -o /home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/runnable
-/home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/runnable > ./Scripts/result.txt
+make -C ./examples/testcode-epcc/
+./examples/testcode-epcc/oa > ./Scripts/result.txt
 
 for i in $(seq 77)
 do
@@ -29,17 +29,19 @@ do
 	for j in "${outerinner[@]}"
 	do
 		loopname+="$j"
-		java -cp "lib/*:bin:../edu.auburn.oaccrefac.core/bin" DistributeLoops ./Scripts/inputtemp.c "$loopname"  > ./Scripts/outputtemp.c 2>> ./Scripts/errorlog.txt	
+		java -cp "lib/*:bin:../edu.auburn.oaccrefac.core/bin" DistributeLoops ./examples/testcode-epcc/inputtemp.c "$loopname"  > ./examples/testcode-epcc/outputtemp.c 2>> ./Scripts/errorlog.txt	
 		if [[ -s ./Scripts/outputtemp.c ]]; then
-			cp ./Scripts/outputtemp.c ./Scripts/inputtemp.c
+			cp ./examples/testcode-epcc/outputtemp.c ./Scripts/inputtemp.c
 		fi
 		echo $loopname
 		loopname=$loopnameTemp
 	done
 done
 
-g++ ./Scripts/output.c -o /home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/runnable
-/home/stupideclipse/Code/oaccrefac/edu.auburn.oaccrefac.ui/testcode-epcc/runnable > result2.txt
+
+
+g++ ./examples/testcode-epcc/output.c -o ./examples/testcode-epcc/runnable
+./examples/testcode-epcc/runnable > result2.txt
 
 if cmp -s result.txt result2.txt
 then
@@ -50,3 +52,5 @@ fi
 
 rm ./Scripts/outputtemp.c
 rm ./Scripts/inputtemp.c
+rm ./examples/testcode-epcc/runnable
+rm ./examples/testcode-epcc/outputtemp.c
