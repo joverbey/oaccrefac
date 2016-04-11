1460430ecfb928f95933d043e62b179da040836d
jwowillo <will.orourke10@gmail.com>
2016-04-11 13:29:42 -0400

    Modify loop unrolling so loop index variable can't be modified within loop.

--- org.eclipse.ptp.pldt.tests/testcode/LoopUnrolling/04_01-ModifiedCounter/modified_counter.c
+++ org.eclipse.ptp.pldt.tests/testcode/LoopUnrolling/04_01-ModifiedCounter/modified_counter.c
@@ -3,9 +3,11 @@ int main() {
 
 	char a[10];
 	char b[10];
-    for (int i = 0; i < 10; i++) { /*<<<<< 6,5,9,6,3,pass*/
-        a[i] = b[i];
+	int d = 0;
+    for (int i = 0; i < 10; i++) { /*<<<<< 6,5,9,6,3,fail*/
         i++;
+        a[i] = b[i];
+        d++;
     }
     return 0;
 }
