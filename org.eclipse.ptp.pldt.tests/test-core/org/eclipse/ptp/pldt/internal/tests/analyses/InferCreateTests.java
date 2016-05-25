19d90984f561bdb013622bf02015d3cbe316b91f
Alexander Calvert <afc0005@auburn.edu>
2016-05-25 15:46:07 -0500

    added some InferCreate tests

--- org.eclipse.ptp.pldt.tests/test-core/org/eclipse/ptp/pldt/internal/tests/analyses/InferCreateTests.java
+++ org.eclipse.ptp.pldt.tests/test-core/org/eclipse/ptp/pldt/internal/tests/analyses/InferCreateTests.java
@@ -19,104 +19,191 @@ import junit.framework.TestCase;
 public class InferCreateTests extends TestCase {
 
 	public void testNothing() throws CoreException {
-		IASTTranslationUnit tu = ASTUtil.translationUnitForString(""
-                + "void main() {                    \n"
-                + "    #pragma acc parallel loop    \n"
-                + "    for(int i = 0; i < 10; i++) {\n"
-                + "        ;//do nothing            \n"
-                + "    }                            \n"
-                + "}");
+		IASTTranslationUnit tu = ASTUtil.translationUnitForString("" + "void main() {                    \n"
+				+ "    #pragma acc parallel loop    \n" + "    for(int i = 0; i < 10; i++) {\n"
+				+ "        ;//do nothing            \n" + "    }                            \n" + "}");
 		IASTFunctionDefinition func = ASTUtil.findOne(tu, IASTFunctionDefinition.class);
 		IASTStatement[] stmts = ((IASTCompoundStatement) func.getBody()).getStatements();
-		Map<IASTStatement, Set<IBinding>> copyins = new InferCopyin(stmts).get(); 
-		for(IASTStatement stmt : copyins.keySet()) {
+		Map<IASTStatement, Set<IBinding>> copyins = new InferCopyin(stmts).get();
+		for (IASTStatement stmt : copyins.keySet()) {
 			assertTrue(copyins.get(stmt).isEmpty());
 		}
 	}
 
 	public void testVarsButNoCreate() throws CoreException {
-		IASTTranslationUnit tu = ASTUtil.translationUnitForString(""
-                + "void main() {                        \n"
-                + "    int a, b;                        \n"
-                + "	   #pragma acc data copyin(a, b)    \n"
-                + "	   {                                \n"
-                + "        #pragma acc parallel loop    \n"
-                + "        for(int i = 0; i < 10; i++) {\n"
-                + "            ;//do nothing            \n"
-                + "        }                            \n"
-                + "    }                                \n"
-                + "}");
+		IASTTranslationUnit tu = ASTUtil.translationUnitForString("" + "void main() {                        \n"
+				+ "    int a, b;                        \n" + "	   #pragma acc data copyin(a, b)    \n"
+				+ "	   {                                \n" + "        #pragma acc parallel loop    \n"
+				+ "        for(int i = 0; i < 10; i++) {\n" + "            ;//do nothing            \n"
+				+ "        }                            \n" + "    }                                \n" + "}");
 		IASTFunctionDefinition func = ASTUtil.findOne(tu, IASTFunctionDefinition.class);
-		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());  
+		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());
 		IASTStatement[] stmts = outer.getStatements();
-		Map<IASTStatement, Set<IBinding>> copyins = new InferCopyin(stmts).get(); 
-		for(IASTStatement stmt : copyins.keySet()) {
+		Map<IASTStatement, Set<IBinding>> copyins = new InferCopyin(stmts).get();
+		for (IASTStatement stmt : copyins.keySet()) {
 			assertTrue(copyins.get(stmt).isEmpty());
 		}
 	}
-	
+
 	public void testSimple() throws CoreException {
-		IASTTranslationUnit tu = ASTUtil.translationUnitForString(""
-                + "void main() {                        \n" //1
-                + "    int a;                           \n" //2
-                + "    int b;                           \n" //3
-                + "	   #pragma acc data copyin(a, b)    \n" //4
-                + "	   {                                \n" //5
-                + "        #pragma acc parallel loop    \n" //6
-                + "        for(int i = 0; i < 10; i++) {\n" //7
-                + "             int tmp = a;            \n" //8
-                + "             b = a;                  \n" //9
-                + "             a = b;                  \n" //10
-                + "             a = tmp;                \n" //11
-                + "        }                            \n" //12
-                + "    }                                \n" //13
-                + "}");
+		IASTTranslationUnit tu = ASTUtil.translationUnitForString("" + "void main() {                        \n" // 1
+				+ "    int a;                           \n" // 2
+				+ "    int b;                           \n" // 3
+				+ "	   #pragma acc data copyin(a, b)    \n" // 4
+				+ "	   {                                \n" // 5
+				+ "        #pragma acc parallel loop    \n" // 6
+				+ "        for(int i = 0; i < 10; i++) {\n" // 7
+				+ "             int tmp = a;            \n" // 8
+				+ "             b = a;                  \n" // 9
+				+ "             a = b;                  \n" // 10
+				+ "             a = tmp;                \n" // 11
+				+ "        }                            \n" // 12
+				+ "    }                                \n" // 13
+				+ "}");
 		IASTFunctionDefinition func = ASTUtil.findOne(tu, IASTFunctionDefinition.class);
-		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());  
+		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());
 		IASTStatement[] stmts = outer.getStatements();
-		Map<IASTStatement, Set<IBinding>> create = new InferCreate(stmts).get(); 
-		for(IASTStatement stmt : create.keySet()) {
-			if(stmt instanceof ArbitraryStatement) {
+		Map<IASTStatement, Set<IBinding>> create = new InferCreate(stmts).get();
+		for (IASTStatement stmt : create.keySet()) {
+			if (stmt instanceof ArbitraryStatement) {
 				assertTrue(containsBinding(create, stmt, "a"));
 				assertTrue(containsBinding(create, stmt, "b"));
 				assertTrue(create.get(stmt).size() == 2);
+			} else {
+				switch (stmt.getFileLocation().getStartingLineNumber()) {
+				case 7:
+					assertTrue(create.get(stmt).isEmpty());
+					break;
+				default:
+					// we should be covering every possible case
+					assertTrue(false);
+					break;
+				}
 			}
-			else {
-				switch(stmt.getFileLocation().getStartingLineNumber()) {
-					case 7:
-						assertTrue(create.get(stmt).isEmpty());
-						break;
-					default:
-						//we should be covering every possible case
-						assertTrue(false);
-						break;
+		}
+	}
+
+	public void testSimpleInitialized() throws CoreException {
+		IASTTranslationUnit tu = ASTUtil.translationUnitForString("" + "void main() {                        \n" // 1
+				+ "    int a = 9;                       \n" // 2
+				+ "    int b = 8;                       \n" // 3
+				+ "	   #pragma acc data copyin(a, b)    \n" // 4
+				+ "	   {                                \n" // 5
+				+ "        #pragma acc parallel loop    \n" // 6
+				+ "        for(int i = 0; i < 10; i++) {\n" // 7
+				+ "             int tmp = a;            \n" // 8
+				+ "             b = a;                  \n" // 9
+				+ "             a = b;                  \n" // 10
+				+ "             a = tmp;                \n" // 11
+				+ "        }                            \n" // 12
+				+ "    }                                \n" // 13
+				+ "}");
+		IASTFunctionDefinition func = ASTUtil.findOne(tu, IASTFunctionDefinition.class);
+		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());
+		IASTStatement[] stmts = outer.getStatements();
+		Map<IASTStatement, Set<IBinding>> create = new InferCreate(stmts).get();
+		for (IASTStatement stmt : create.keySet()) {
+			if (stmt instanceof ArbitraryStatement) {
+				assertTrue(containsBinding(create, stmt, "b"));
+				assertTrue(create.get(stmt).size() == 1);
+			} else {
+				switch (stmt.getFileLocation().getStartingLineNumber()) {
+				case 7:
+					assertTrue(create.get(stmt).isEmpty());
+					break;
+				default:
+					// we should be covering every possible case
+					assertTrue(false);
+					break;
+				}
+			}
+		}
+	}
+
+	public void testSimpleArrays() throws CoreException {
+		IASTTranslationUnit tu = ASTUtil.translationUnitForString("" + "void main() {                        \n" // 1
+				+ "    int a[3];                        \n" // 2
+				+ "    int b[3];                        \n" // 3
+				+ "	   #pragma acc data copyin(a, b)    \n" // 4
+				+ "	   {                                \n" // 5
+				+ "        #pragma acc parallel loop    \n" // 6
+				+ "        for(int i = 0; i < 10; i++) {\n" // 7
+				+ "             int tmp = a[i];         \n" // 8
+				+ "             b[i] = a[i];            \n" // 9
+				+ "             a[i] = b[i];            \n" // 10
+				+ "             a[i] = tmp;             \n" // 11
+				+ "        }                            \n" // 12
+				+ "    }                                \n" // 13
+				+ "}");
+		IASTFunctionDefinition func = ASTUtil.findOne(tu, IASTFunctionDefinition.class);
+		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());
+		IASTStatement[] stmts = outer.getStatements();
+		Map<IASTStatement, Set<IBinding>> create = new InferCreate(stmts).get();
+		for (IASTStatement stmt : create.keySet()) {
+			if (stmt instanceof ArbitraryStatement) {
+				assertTrue(containsBinding(create, stmt, "a"));
+				assertTrue(containsBinding(create, stmt, "b"));
+				assertTrue(create.get(stmt).size() == 2);
+			} else {
+				switch (stmt.getFileLocation().getStartingLineNumber()) {
+				case 7:
+					assertTrue(create.get(stmt).isEmpty());
+					break;
+				default:
+					// we should be covering every possible case
+					assertTrue(false);
+					break;
 				}
 			}
 		}
 	}
 	
-	
-	private boolean containsBinding(Map<IASTStatement, Set<IBinding>> creates, IASTStatement statement, String binding) {
+	public void testSimpleArraysInitialized() throws CoreException {
+		IASTTranslationUnit tu = ASTUtil.translationUnitForString("" + "void main() {                        \n" // 1
+				+ "    int a[3] = {1, 2, 3};            \n" // 2
+				+ "    int b[3] = {4, 5, 6};            \n" // 3
+				+ "	   #pragma acc data copyin(a, b)    \n" // 4
+				+ "	   {                                \n" // 5
+				+ "        #pragma acc parallel loop    \n" // 6
+				+ "        for(int i = 0; i < 10; i++) {\n" // 7
+				+ "             int tmp = a[i];         \n" // 8
+				+ "             b[i] = a[i];            \n" // 9
+				+ "             a[i] = b[i];            \n" // 10
+				+ "             a[i] = tmp;             \n" // 11
+				+ "        }                            \n" // 12
+				+ "    }                                \n" // 13
+				+ "}");
+		IASTFunctionDefinition func = ASTUtil.findOne(tu, IASTFunctionDefinition.class);
+		IASTCompoundStatement outer = getFirstChildCompound(func.getBody());
+		IASTStatement[] stmts = outer.getStatements();
+		Map<IASTStatement, Set<IBinding>> create = new InferCreate(stmts).get();
+		for (IASTStatement stmt : create.keySet()) {
+			assertTrue(create.get(stmt).isEmpty());
+		}
+	}
+
+	private boolean containsBinding(Map<IASTStatement, Set<IBinding>> creates, IASTStatement statement,
+			String binding) {
 		Set<IBinding> bindings = creates.get(statement);
-		for(IBinding b : bindings) {
-			if(b.getName().equals(binding)) {
+		for (IBinding b : bindings) {
+			if (b.getName().equals(binding)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	private IASTCompoundStatement getFirstChildCompound(IASTStatement statement) {
-		if(!(statement instanceof IASTCompoundStatement)) {
+		if (!(statement instanceof IASTCompoundStatement)) {
 			throw new IllegalArgumentException();
 		}
-		
-		for(IASTStatement s : ((IASTCompoundStatement) statement).getStatements()) {
-			if(s instanceof IASTCompoundStatement) {
+
+		for (IASTStatement s : ((IASTCompoundStatement) statement).getStatements()) {
+			if (s instanceof IASTCompoundStatement) {
 				return (IASTCompoundStatement) s;
 			}
 		}
 		return null;
 	}
-	
+
 }
