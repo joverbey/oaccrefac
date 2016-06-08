3c102e10af96790c6a9252bba4fd7a164b055ae8
Raiden <crw0034@auburn.edu>
2016-06-08 12:24:44 -0500

    Added some test cases for interchange

---
+++ org.eclipse.ptp.pldt.tests/testcode/InterchangeLoops/09-IncrementByThree/incrementbythree.c
@@ -0,0 +1,10 @@
+int main() {
+
+	int N = 100;
+	int a[N];
+	for (int i = 0; i < N; i += 3) {
+		for (int j = 0; j < N; j++) { /*<<<<< 5,1,9,1,1,pass*/
+			a[i] = 0;
+		}
+	}
+}
---
+++ org.eclipse.ptp.pldt.tests/testcode/InterchangeLoops/12-Long/long.c
@@ -0,0 +1,10 @@
+int main() {
+
+	int N = 100;
+	int a[N];
+	for (long i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) { /*<<<<< 5,1,9,3,1,pass*/
+			a[i] = 0;
+		}
+	}
+}
