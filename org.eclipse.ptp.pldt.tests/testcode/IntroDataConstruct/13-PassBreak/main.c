4a7112c26470bfd251db94deea0fe245cc153ab3
Alexander Calvert <afc0005@auburn.edu>
2016-06-14 15:19:10 -0500

    added IntroData tests for control flow statements

---
+++ org.eclipse.ptp.pldt.tests/testcode/IntroDataConstruct/13-PassBreak/main.c
@@ -0,0 +1,31 @@
+int main() {
+
+	int i, a, b;
+
+	b = 16; /*<<<<< 6,0,19,0,pass*/
+
+#pragma acc parallel
+	{
+#pragma acc loop
+    for (i = 0; i < 100; i++) {
+        a = i + b;
+        if(a > 100) break;
+    }
+
+#pragma acc loop
+    for (i = 0; i < 100; i++)
+        a = i + 10;
+	}
+
+	printf("%d\n", a + 12);
+
+	if(a == 10) {
+		printf("Success!\n");
+	}
+	else {
+		printf("Failure!");
+	}
+
+	return 0;
+
+}
