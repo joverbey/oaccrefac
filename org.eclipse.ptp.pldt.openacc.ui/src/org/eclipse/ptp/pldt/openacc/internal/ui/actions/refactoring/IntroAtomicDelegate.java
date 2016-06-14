05570e905d12ce01df5eafb3daa04f63c76a3900
Jeffrey Overbey <com-github@jeff.over.bz>
2016-06-14 11:19:08 -0500

    Move Intro Atomic in menu; rename classes for consistency

---
+++ org.eclipse.ptp.pldt.openacc.ui/src/org/eclipse/ptp/pldt/openacc/internal/ui/actions/refactoring/IntroAtomicDelegate.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Auburn University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     William Hester (Auburn) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ptp.pldt.openacc.internal.ui.actions.refactoring;
+
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.core.model.IWorkingCopy;
+import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.ltk.core.refactoring.Refactoring;
+import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
+import org.eclipse.ptp.pldt.openacc.internal.ui.LoopRefactoringWizard;
+import org.eclipse.ptp.pldt.openacc.internal.ui.refactorings.IntroAtomicRefactoring;
+
+@SuppressWarnings("restriction")
+public class IntroAtomicDelegate extends RefactoringActionDelegate {
+
+    @Override
+    public CRefactoring createRefactoring(IWorkingCopy wc, ITextSelection selection, ICProject project) {
+        return new IntroAtomicRefactoring(wc, selection, project);
+    }
+
+    @Override
+    public RefactoringWizard createWizard(Refactoring refactoring) {
+        return new LoopRefactoringWizard(refactoring, "Introduce OpenACC Atomic");
+    }
+
+}
